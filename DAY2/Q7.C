c312aea09af6b3c14f465b7cbcb1551e993a66af
Ankush Singh <25116cn447@glbitm.ac.in>
2026-06-10 10:10:29 +0530

    ADDED DAY2

---
+++ DAY2/Q7.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n, prod = 1;
+    scanf("%d", &n);
+    while(n > 0) {
+        prod *= n % 10;
+        n /= 10;
+    }
+    printf("%d", prod);
+    return 0;
+}
