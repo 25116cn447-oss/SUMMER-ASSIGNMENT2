936ef394f837e15bde8610e29b57c2ff65e6a76e
Ankush Singh <25116cn447@glbitm.ac.in>
2026-06-14 19:56:03 +0530

    ADDED DAY 14

---
+++ DAY 14/Q54.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, key, count = 0;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key)
+            count++;
+    }
+
+    printf("Frequency = %d", count);
+
+    return 0;
+}
