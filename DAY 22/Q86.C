eeb623a4c969b77a3275789bfe9bebf0a581f93c
Ankush Singh <25116cn447@glbitm.ac.in>
2026-06-23 22:04:08 +0530

    ADDED DAY 22

---
+++ DAY 22/Q86.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i, words = 1;
+
+    gets(str);
+
+    for(i=0; str[i]!='\0'; i++) {
+        if(str[i] == ' ')
+            words++;
+    }
+
+    printf("Total Words = %d", words);
+
+    return 0;
+}
